0bed736b94b52f70ca8c7cd78db4e3dadb24994a
Przemek Sipta <przemek.sipta@gmail.com>
2021-07-08 19:56:32 +0200

    Validate user before register and fix hash password

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,20 +1,12 @@
-import { Request, Response } from "express";
-import User from "../models/userModel";
-import { UserType } from "../types/userTypes";
+import { Request, Response } from 'express';
+import User from '../models/userModel';
+import { UserType } from '../types/userTypes';
 
-export async function checkUserExist(
-  req: Request<any, any, UserType>,
-  res: Response,
-  next: () => void
-) {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (user) {
-      res.status(400).json("User exist.");
-    } else {
-      next();
-    }
-  } catch (error) {
-    res.status(500).json(error);
-  }
+export async function checkUserExist(email: string) {
+	try {
+		const user = await User.findOne({ email });
+		return !!user;
+	} catch (error) {
+		return false;
+	}
 }
